b8217ae1213e3af283b426e044e4d9b645169c24
andrii <andrii.sukhoi@nocowanie.pl>
2021-03-06 10:17:14 +0100

    some front-end stuff

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <header>
+        test
+    </header>
+</div>
+</body>
+</html>
