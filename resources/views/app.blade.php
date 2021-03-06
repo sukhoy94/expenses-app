d5289ef81c795850001360be3028253aaff48866
andrii <andrii.sukhoi@nocowanie.pl>
2021-03-06 10:42:51 +0100

    add auth

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,10 +8,29 @@
     <title>Document</title>
 </head>
 <body>
-<div>
-    <header>
-        test
-    </header>
+<div class="container mx-auto">
+    <table class="border-collapse border border-green-800">
+        <thead>
+        <tr>
+            <th class="border border-green-600">Label</th>
+            <th class="border border-green-600">Amount</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="border border-green-600">Indiana</td>
+            <td class="border border-green-600">Indianapolis</td>
+        </tr>
+        <tr>
+            <td class="border border-green-600">Ohio</td>
+            <td class="border border-green-600">Columbus</td>
+        </tr>
+        <tr>
+            <td class="border border-green-600">Michigan</td>
+            <td class="border border-green-600">Detroit</td>
+        </tr>
+        </tbody>
+    </table>
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Auth::routes();
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
