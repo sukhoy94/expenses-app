ef8650e582be738b1a3eb1a54fa18e2db09f1d24
andrii <andrii.sukhoi@nocowanie.pl>
2021-11-06 09:25:04 +0100

    make submit butto full-width on mobile

--- resources/views/expenses/edit.blade.php
+++ resources/views/expenses/edit.blade.php
@@ -18,54 +18,59 @@
 <form method="POST" action="{{route('expenses.update', ['expense' => $expense->id])}}">
     @csrf
     @method('PUT')
-        {{-- todo: move this form to component, as it is the same as on index.blade.php view --}}
-        <div class="row">
-            <div class="col-md-12 form-group">
-                <label for="expenseCategory">Category</label>
-                <select class="form-control" name="category" id="expenseCategory">
-                    @foreach($categories as $category)
-                        <option @if($expense->category_id == $category->id) selected @endif value="{{ $category->id }}">{{ $category->title }}</option>
-                    @endforeach
-                </select>
-            </div>   
-    
-            <div class="col-md-12 form-group">
-                <label for="expendedTitle">Title</label>
-                <input
-                    type="text"
-                    class="form-control"
-                    id="expendedTitle"
-                    placeholder="Title"
-                    name="expendedAmountTitle"
-                    value="{{ $expense->title }}"
-                >
-            </div>
-        
-            <div class="col-md-6 form-group">
-                <label for="expendedInput">Expended</label>
-                <input
-                        type="number"
-                        class="form-control"
-                        id="expendedInput"
-                        placeholder="Enter how much money was expended"
-                        name="expendedAmount"
-                        step="0.01"
-                        value="{{ $expense->amount }}"
-                >            
-            </div>
-        
-            <div class="col-md-6 form-group">
-                <label for="dateInput">Date</label>
-                <input
-                        type="datetime-local"
-                        class="form-control"
-                        id="dateInput"
-                        name="expendedDate"
-        
-                        value="{{ $expenseCreatedAtFormatted }}"
-                >
-            </div>
+    {{-- todo: move this form to component, as it is the same as on index.blade.php view --}}
+    <div class="row">
+        <div class="col-md-12 form-group">
+            <label for="expenseCategory">Category</label>
+            <select class="form-control" name="category" id="expenseCategory">
+                @foreach($categories as $category)
+                    <option @if($expense->category_id == $category->id) selected @endif value="{{ $category->id }}">{{ $category->title }}</option>
+                @endforeach
+            </select>
+        </div>   
+
+        <div class="col-md-12 form-group">
+            <label for="expendedTitle">Title</label>
+            <input
+                type="text"
+                class="form-control"
+                id="expendedTitle"
+                placeholder="Title"
+                name="expendedAmountTitle"
+                value="{{ $expense->title }}"
+            >
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+    
+        <div class="col-md-6 form-group">
+            <label for="expendedInput">Expended</label>
+            <input
+                    type="number"
+                    class="form-control"
+                    id="expendedInput"
+                    placeholder="Enter how much money was expended"
+                    name="expendedAmount"
+                    step="0.01"
+                    value="{{ $expense->amount }}"
+            >            
+        </div>
+    
+        <div class="col-md-6 form-group">
+            <label for="dateInput">Date</label>
+            <input
+                    type="datetime-local"
+                    class="form-control"
+                    id="dateInput"
+                    name="expendedDate"
+    
+                    value="{{ $expenseCreatedAtFormatted }}"
+            >
+        </div>
+
+        <div class="col-sm-3 col-xs-12 form-group">
+            <button type="submit" class="btn btn-primary btn-block">
+                Submit
+            </button>
+        </div>
+    </div>
 </form>
 @endsection
